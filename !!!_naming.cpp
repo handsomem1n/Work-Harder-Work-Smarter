dad6e7177445f8c201647f343a50918e29a50b69
handsomemin <128664961+handsomeminzz@users.noreply.github.com>
2023-04-13 16:43:16 +0900

    cpp

---
+++ !!!_naming.cpp
@@ -0,0 +1,9 @@
+int myVariable; // 변수 이름은 소문자로 시작하는 CamelCase 형식
+const int MY_CONSTANT = 10; // 상수 이름은 모두 대문자로 작성
+void myFunction() { // 함수 이름은 소문자로 시작하는 CamelCase 형식
+  // 함수 내용
+}
+
+class MyClass { // 클래스 이름은 첫 글자를 대문자로 작성하고, CamelCase 형식을 사용
+  // 클래스 멤버 등
+};
